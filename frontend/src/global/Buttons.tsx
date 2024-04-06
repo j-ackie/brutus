07c7f7a9ac958dfc6ed4f76732b6855033a2e683
kyeoul <54390304+kyeoul@users.noreply.github.com>
2024-04-06 14:38:04 -0500

    Update Button.tsx

---
+++ frontend/src/global/Buttons.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function IconButton() {
+  return (
+    <div>
+      IconButton
+    </div>
+  )
+}
+
+function TextButton() {
+  return (
+    <div>
+      TextButton
+    </div>
+  )
+}
+
+export { IconButton, TextButton }
