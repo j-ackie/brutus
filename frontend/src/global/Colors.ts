cc0773abb055281346be77bf44fb74766584316a
dpark4 <davidpark2025@u.northwestern.edu>
2024-04-06 18:46:43 -0500

    colors

--- frontend/src/global/Colors.ts
+++ frontend/src/global/Colors.ts
@@ -1,32 +1,3 @@
-
-function hexToRGB(hex : string) {
-  let r = 0, g = 0, b = 0;
-  // 3 digits
-  if (hex.length === 4) {
-    r = parseInt(hex[1] + hex[1], 16);
-    g = parseInt(hex[2] + hex[2], 16);
-    b = parseInt(hex[3] + hex[3], 16);
-  }
-  // 6 digits
-  else if (hex.length === 7) {
-    r = parseInt(hex[1] + hex[2], 16);
-    g = parseInt(hex[3] + hex[4], 16);
-    b = parseInt(hex[5] + hex[6], 16);
-  }
-  return { r, g, b };
-}
-
-function rgbaToHex(r: number, g: number, b: number, a: number): string {
-  const red = Math.max(0, Math.min(255, r));
-  const green = Math.max(0, Math.min(255, g));
-  const blue = Math.max(0, Math.min(255, b));
-  const alpha = Math.round(Math.max(0, Math.min(1, a)) * 255).toString(16).padStart(2, '0');
-  return `#${red.toString(16).padStart(2, '0')}${green.toString(16).padStart(2, '0')}${blue.toString(16).padStart(2, '0')}${alpha}`;
-}
-
-const accentColorRGB = hexToRGB('#2c44c7');
-const accentColorHex = rgbaToHex(accentColorRGB.r, accentColorRGB.g, accentColorRGB.b, 0.45);
-
 const COLORS = {
   // white
   'text': "#f8f0f0",
@@ -45,7 +16,7 @@ const COLORS = {
   // paradise pink
   'accent3': "#ed3d63",
   // light blue
-  'lightaccent': accentColorHex, 
+  'lightblue': '#a2aae1', 
   // red button
   'red': "#dc3545",
   // green button
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Listing from '../../components/Listing/Listing'
-import {listingQuery} from '../../pages/home/homeQueries'
+import { listingQuery } from '../../pages/home/homeQueries'
+import { COLORS } from '../../global/Colors'
+
 function Home() {
 
   const listing = listingQuery()
 
   return (
-    <div>
+    <div style = {
+      {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: COLORS.primary
+      }
+    }>
+      
       <Listing {...listing}/>
     </div>
   )
